39d75d23718306037180848211c0fd71dc21a600
morgan-mcnabb <morgan.mcnabb@protonmail.com>
2024-04-26 20:46:14 -0400

    got backend communicating with frontend

---
+++ WeatherDashboard/WeatherDashboard/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherDashboard.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("testendpoint")]
+        public IActionResult GetTest()
+        {
+            var response = new { message = "Backend and frontend are communicating!"};
+            return Ok(response);
+        }
+    }
+}
--- WeatherDashboard/WeatherDashboard/Program.cs
+++ WeatherDashboard/WeatherDashboard/Program.cs
@@ -1,14 +1,31 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+        builder.WithOrigins("http://localhost:3000") // Adjust this as necessary for your React app's URL
+               .AllowAnyHeader()
+               .AllowAnyMethod());
+});
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseCors(); // Apply CORS policy
 
 app.UseAuthorization();
 
